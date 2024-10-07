346f2dd5a6907921902ec7c7c3bd5d7538ef4bf5
Abhinay Kumar <101319759+abhinay-techx@users.noreply.github.com>
2024-10-07 06:02:40 +0000

    Complted

--- day5/songify/queries/8.sql
+++ day5/songify/queries/8.sql
@@ -1,2 +1,6 @@
 -- Question 8: I want to know if any of these songs were released in the last 4 months of 2023. 
 -- Provide the track and artist names, along with the date of release.
+SELECT track_name, artist_names, release_date
+FROM spotify_tracks
+WHERE strftime('%Y', release_date) = '2023'
+  AND strftime('%m', release_date) IN ('09', '10', '11', '12');
